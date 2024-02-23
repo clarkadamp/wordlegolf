a97e9bf39a9dcd96356ded70cb9c196dbe428816
Adam Clark <cladam@amazon.com>
2024-02-23 14:38:28 +1100

    refactor, add in full random method, cli args

--- letters.py
+++ letters.py
@@ -1,32 +1,78 @@
 import random
+import sys
+from argparse import ArgumentParser, Namespace
+from typing import Callable, Iterable, Mapping, Sequence, Tuple
+
+LetterGernerator = Callable[[bool], Iterable[Tuple[str, str]]]
 
 all_letters = list(map(chr, range(ord("a"), ord("z") + 1)))
 
-
-adam_heather = ["adam", "heather"]
-random.shuffle(adam_heather)
-
-players = ["danc", "danz", "ina", "ross"]
-random.shuffle(players)
-
-team_a = adam_heather[:1] + players[:2]
-team_b = adam_heather[1:] + players[2:]
+GENERATORS: Mapping[str, LetterGernerator] = {}
 
 
-lotd = all_letters.copy()
-bonus = all_letters.copy()
-random.shuffle(lotd)
-random.shuffle(bonus)
+def random_letters() -> Sequence[str]:
+    letters = all_letters.copy()
+    random.shuffle(letters)
+    return letters
 
 
-print(f"Team A: {', '.join(sorted(team_a))}")
-print(f"Team B: {', '.join(sorted(team_b))}")
+def generator(func: LetterGernerator) -> LetterGernerator:
+    GENERATORS[func.__name__.replace("_", "-")] = func
+    return func
 
-hole: int = 0
-for d, b in zip(lotd, bonus):
-    if d == b:
-        continue
-    hole += 1
-    print(f"Hole: {hole}, LOTD: {d.upper()} Bonus: {b.upper()}")
-    if hole >= 18:
-        break
+
+def skins_teams() -> None:
+    adam_heather = ["adam", "heather"]
+    random.shuffle(adam_heather)
+
+    players = ["danc", "danz", "ina", "ross"]
+    random.shuffle(players)
+
+    team_a = adam_heather[:1] + players[:2]
+    team_b = adam_heather[1:] + players[2:]
+
+    print(f"Team A: {', '.join(sorted(team_a))}")
+    print(f"Team B: {', '.join(sorted(team_b))}")
+
+
+@generator
+def no_repeat(allow_same: bool) -> Iterable[Tuple[str, str]]:
+    for daily, bonus in zip(random_letters(), random_letters()):
+        if not allow_same and daily == bonus:
+            continue
+        yield daily, bonus
+
+
+@generator
+def full_random(allow_same: bool) -> Iterable[Tuple[str, str]]:
+    while True:
+        daily = random_letters()[0]
+        bonus = random_letters()[0]
+        if not allow_same and daily == bonus:
+            continue
+        yield daily, bonus
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("--generator", choices=sorted(GENERATORS), default="no-repeat")
+    parser.add_argument("--allow-same", action="store_true", default=False)
+    parser.add_argument("--skins", action="store_true", default=False)
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+
+    if args.skins:
+        skins_teams()
+
+    print(f"Mode: {' '.join(sys.argv[1:])}")
+    for hole, (d, b) in enumerate(GENERATORS[args.generator](args.allow_same), 1):
+        print(f"Hole: {hole}, LOTD: {d.upper()} Bonus: {b.upper()}")
+        if hole >= 18:
+            break
+
+
+if __name__ == "__main__":
+    main()
